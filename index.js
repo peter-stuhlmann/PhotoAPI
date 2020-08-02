cf1e95c0bfabd69242899193e38c96b79c8d8117
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-08-02 20:57:27 +0200

    Add body parser

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express();
 const port = 3112;
 const v1 = require('./v1');
 const connectMongo = require('./db/db');
+const bodyParser = require('body-parser');
 
 try {
   connectMongo();
 } catch (e) {
   console.log('Error!!!');
 }
+
+app.use(bodyParser.json());
 
 app.use('/v1', v1);
 
